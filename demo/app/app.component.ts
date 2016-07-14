d6d4e69ba7bbc64c8b61701ffca28b3f157c9f4a
Michael Geyser <michaelg@bbd.co.za>
2016-07-14 11:25:02 +0200

    2 - Render a list of todos.

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
 
+class Todo {
+    title: string;
+    completed: boolean;
+
+    constructor(title: string, completed: boolean) {
+        this.title = title;
+        this.completed = completed;
+    }
+}
+
 @Component({
     selector: 'todo-app',
-    template: '<h1>Hello JSinSA!</h1>'
+    templateUrl: 'app/app.template.html'
 })
 export class AppComponent {
-    
+    todos: Todo[];
+
+    constructor() {
+        this.todos = [
+            new Todo("List todos", true), // This is done!
+            new Todo("Add conditional styling", true),
+            new Todo("Mark a todo as completed", false),
+            new Todo("Add a todo", false),
+            new Todo("Delete a todo", false),
+            new Todo("Edit a todo", false),
+            new Todo("???", true), // Huh?
+            new Todo("Profit", false)
+        ];
+    }
 }
